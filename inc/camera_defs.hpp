a2ea4cffd4b6454c37a939d6892e63bb1bae3d18
blr-mac <blueslugger48@gmail.com>
2026-08-03 12:32:58 -0700

    back to normal

--- inc/camera_defs.hpp
+++ inc/camera_defs.hpp
@@ -5,3 +5,4 @@
 //if io-mode=2 not supported (older/worse camera)
 inline std::string gst_cam1 = "v4l2src device=/dev/video0 ! image/jpeg, width=1920, height=1080, framerate=30/1 ! jpegparse ! nvv4l2decoder mjpeg=1 ! nvvidconv ! video/x-raw, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink name=appsink drop=true sync=false";
 inline std::string gst_cam2 = "v4l2src device=/dev/video2 ! image/jpeg, width=1920, height=1080, framerate=30/1 ! jpegparse ! nvv4l2decoder mjpeg=1 ! nvvidconv ! video/x-raw, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink name=appsink drop=true sync=false";
+
